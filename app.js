c0c2b72c3591e5e548b994485a36a3b3a6a8495d
Dan Araujo <danershome@gmail.com>
2019-02-20 00:26:07 -0300

    Update.

--- app.js
+++ app.js
@@ -9,20 +9,7 @@ class Item {
 //Classe da interface
 class UI {
 	static displayItems(){
-		const StoredItems = [
-			{
-				title: "Item Um",
-				desc: "Este é o primeiro item",
-				detail: "E aqui vamos ter detalhes do item"
-			},
-			{
-				title: "Item Dois",
-				desc: "Este é o segundo item",
-				detail: "E aqui vamos ter detalhes do item"
-			}
-		];
-
-		const items = StoredItems;
+		const items = Store.getItems();
 
 		items.forEach((item) => UI.addItemToList(item));
 	}
@@ -42,6 +29,34 @@ class UI {
 		item_list.appendChild(row);
 	}
 
+	static deleteItem(el){
+		if (el.classList.contains("delete")) {
+			el.parentElement.parentElement.remove();
+		}
+	}
+
+	static showAlerts(msg, type, title){
+		if (type == 'error') {
+			iziToast.error({
+				title: title,
+				message: msg
+			});	
+		}
+		if (type == 'success') {
+			iziToast.success({
+				title: title,
+				message: msg
+			});	
+		}
+		if (type == 'warning') {
+			iziToast.warning({
+				title: title,
+				message: msg
+			});	
+		}
+		
+	}
+
 	static clearItems(){
 		document.querySelector("#title").value = "";
 		document.querySelector("#desc").value = "";
@@ -49,7 +64,35 @@ class UI {
 	}
 }
 //Classe do armazenamento
+class Store{
+	static getItems(){
+		let items;
 
+		if (localStorage.getItem("items") === null) {
+			items = [];
+		} else {
+			items = JSON.parse(localStorage.getItem("items"));
+		}
+
+		return items;
+	}
+
+	static addItem(item){
+		const items = Store.getItems();
+		items.push(item);
+		localStorage.setItem("items", JSON.stringify(items));
+	}
+
+	static removeItem(detail){
+		const items = Store.getItems();
+		items.forEach((item, index) => {
+			if (item.detail === detail) {
+				items.splice(index, 1);
+			}
+		});
+		localStorage.setItem("items", JSON.stringify(items));
+	}
+}
 //Evento READ
 document.addEventListener("DOMContentLoaded", UI.displayItems)
 //Evento CREATE
@@ -61,9 +104,21 @@ document.querySelector("#item-form").addEventListener("submit", (e) => {
 	const desc = document.querySelector("#desc").value;
 	const detail = document.querySelector("#detail").value;
 
-	const item = new Item(title, desc, detail);
-	
-	UI.addItemToList(item);
-	UI.clearItems();
+	if (title === "" || desc === "" || detail === "") {
+		UI.showAlerts("Todos campos são obrigatórios!", "error", "Ops");
+		return false;
+	} else {
+		const item = new Item(title, desc, detail);
+		UI.addItemToList(item);
+		Store.addItem(item);
+		UI.clearItems();
+		UI.showAlerts("Gravado com sucesso!", "success", "Oba");
+	}
+
 });
 //Evento DELETE
+document.querySelector("#item-list").addEventListener("click", (e) =>{
+	UI.deleteItem(e.target);
+	Store.removeItem(e.target.parentElement.previousElementSibling.textContent);
+	UI.showAlerts("Excluído com sucesso!", "warning", "Ok");
+});
